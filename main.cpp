91514f843fc7a8f2670fb3f2e1a98a5884e66482
beppe <manzo.giuseppe@gmail.com>
2023-12-17 12:39:15 +0100

    remove input file

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@
 
 using count_t = unsigned long;
 
-void acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g) {
+void acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g, count_t &t) {
 	if( s.starts_with("[Result") ) {
 		g++;
 		auto i = s.find_first_of('-', 9);
+		if( i == s.npos ) {
+			t++;
+			return;
+		}
 		auto c = s.at(i - 1);
-
 		switch(c) {
 			case '1':
 				w++;
@@ -22,34 +25,37 @@ void acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t
 				d++;
 				break;
 		}
+
 	}
 }
 
 int main(int argc, char *argv[]) {
 
-	if( argc < 2) {
-		std::cerr << argv[0] << " <file.pgn>" << std::endl;
-		return 1;
-	}
-
-	std::ifstream inputfile(argv[1]);
+	std::istream *inputfile = &std::cin;
 
 	std::string s0;
 
-	count_t white, black, draw, games;
-	white = black = draw = games = 0;
+	count_t white, black, draw, games, stars;
+	white = black = draw = games = stars = 0;
 
 	do{
-		std::getline(inputfile, s0);
-		acc_resut(s0, white, black, draw, games);
-	} while( inputfile.good() );
+		std::getline(*inputfile, s0);
+		acc_resut(s0, white, black, draw, games, stars);
+		
+	} while( inputfile->good() );
 
-	std::cout << "Games: " << games << "\n"
+	if( inputfile->bad() ) {
+		std::cerr << "Input file critical error" << std::endl;
+		return 1;
+	}
+
+	std::cout << "\nGames: " << games << "\n"
 		<< "White: " << white
 		<< ". Black: " << black
-		<< ". Draw: " << draw  << std::endl;
+		<< ". Draw: " << draw
+		<< ". Unknown: " << stars << std::endl;
 
-	if( games != ( white + black + draw ) ) {
+	if( games != ( white + black + draw + stars ) ) {
 		std::cout << "Warning: games count!" << std::endl;
 	}
 
