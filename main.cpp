ef97b981d547b24a8c9dc0feb39eb324e69adc37
beppe <manzo.giuseppe@gmail.com>
2023-12-21 15:35:06 +0100

    sqlite

--- main.cpp
+++ main.cpp
@@ -15,13 +15,54 @@ struct config {
     std::string output;
 };
 
-using count_t = unsigned long;
+struct sqlite3_deleter {
 
-void acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g, count_t &t);
+    void operator()(sqlite3 *db) const {
+        sqlite3_close(db);
+    }
+};
+
+using count_t = unsigned long;
+using sqlite_ptr = std::unique_ptr<sqlite3,sqlite3_deleter>;
+#define RESULT_WHITE 119
+#define RESULT_BLACK 98
+#define RESULT_DRAW  100
+
+const char * const CREATE_DDL =
+    "CREATE TABLE games("
+        "id     INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "result CHAR(1) NOT NULL"
+    ");";
+
+const char * const INSERT_DML = 
+    "INSERT INTO games(result) VALUES(?);";
+
+int acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g, count_t &t);
 
 void parse_options(config &conf, int argc, char *argv[]);
 
+sqlite_ptr open_db(const std::string &c);
+
+void dump_result(sqlite_ptr &db, int result);
+
 int main(int argc, char *argv[]) {
+
+    config conf;
+
+    parse_options(conf, argc, argv);
+
+    auto db = open_db(conf.output);
+    
+    if( db == nullptr) {
+       exit(2);
+    }
+
+    sqlite3_stmt *stmt;
+    int result = sqlite3_prepare(db.get(), INSERT_DML, -1, &stmt, nullptr);
+    if( result != SQLITE_OK ) {
+        std::cerr << "PREPARE: " << sqlite3_errstr(result) << "\n";
+        exit(2);
+    }
 
 	size_t done = 0;
 
@@ -34,12 +75,23 @@ int main(int argc, char *argv[]) {
 
 	do{
 		std::getline(*inputfile, s0);
-		acc_resut(s0, white, black, draw, games, stars);		
+		result = acc_resut(s0, white, black, draw, games, stars);		
 		done += s0.size() + 1;
-		if( done % 1000000 == 0 ) {
-			//verbose_print(std::format("\rParsed {} MBytes", done/1000000));
-		}
+        result = sqlite3_bind_int( stmt, 1, result);
+        if( result != SQLITE_OK ) {
+            std::cerr << "BIND: " << sqlite3_errstr(result) << "\n";
+            exit(1);
+        }
+        result = sqlite3_step( stmt );
+        if( result != SQLITE_DONE ) {
+            std::cerr << "STEP: " << sqlite3_errstr(result) << "\n";
+            exit(1);
+        }
+        sqlite3_reset(stmt);
+        sqlite3_clear_bindings(stmt);
 	} while( inputfile->good() );
+
+    sqlite3_finalize(stmt);
 
 	if( inputfile->bad() ) {
 		std::cerr << "Input file critical error" << std::endl;
@@ -59,37 +111,58 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g, count_t &t) {
+int acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g, count_t &t) {
 	if( s.starts_with("[Result") ) {
 		g++;
 		auto i = s.find_first_of('-', 9);
 		if( i == s.npos ) {
 			t++;
-			return;
+			return -1;
 		}
 		auto c = s.at(i - 1);
 		switch(c) {
 			case '1':
 				w++;
-				break;
+				return RESULT_WHITE;
 			case '0':
 				b++;
-				break;
+				return RESULT_BLACK;
 			case '2':
 				d++;
-				break;
+				return RESULT_DRAW;
 		}
 
 	}
+    return 0;
 }
 
 void parse_options(config &conf, int argc, char *argv[]) {
 	cxxopts::Options opts("","");
 	opts.add_options()
-		("c,create-script", "Database creation script in case of new dump file", cxxopts::value<std::string>()->default_value("ddl.sql"))
         ("output", "Database file to dump data. If new requires option --create-script", cxxopts::value<std::string>()->default_value("pgn.dump"));
 
 	auto res = opts.parse(argc, argv);
 
-	conf.createscript = res["create-script"].as<std::string>();
+    conf.output = std::move(res["output"].as<std::string>());
 }
+
+sqlite_ptr open_db(const std::string &c) {
+
+    sqlite3 *db;
+    int res = sqlite3_open_v2(c.c_str(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr);
+    if( res != SQLITE_OK ) {
+        std::cerr << sqlite3_errstr(res) << "\n";
+        return nullptr;
+    }
+
+    res = sqlite3_exec(db, CREATE_DDL, nullptr, nullptr, nullptr);
+    if( res != SQLITE_OK ) {
+        std::cerr << sqlite3_errstr(res) << "\n";
+        return nullptr;
+    }
+
+    return sqlite_ptr(db);
+    
+}
+
+
