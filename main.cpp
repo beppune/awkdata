46d962e13e79a6564efab368790cfa81e949314d
beppe <manzo.giuseppe@gmail.com>
2023-12-21 17:27:34 +0100

    unique ptr to statement

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "cxxopts.hpp"
 
 extern "C" {
-	#include "sqlite3/sqlite3.h"
+#include "sqlite3/sqlite3.h"
 }
 
 struct config {
@@ -19,29 +19,42 @@ struct sqlite3_deleter {
 
     void operator()(sqlite3 *db) const {
         sqlite3_close(db);
+        db = nullptr;
+    }
+};
+
+struct stmt_finalizer {
+
+    void operator()(sqlite3_stmt *p) {
+        sqlite3_finalize(p);
+        p = nullptr;
     }
 };
 
 using count_t = unsigned long;
-using sqlite_ptr = std::unique_ptr<sqlite3,sqlite3_deleter>;
+using sqlite_ptr = std::unique_ptr<sqlite3, sqlite3_deleter>;
+using stmt_ptr = std::unique_ptr<sqlite3_stmt, stmt_finalizer>;
+
 #define RESULT_WHITE 1
 #define RESULT_BLACK 2
 #define RESULT_DRAW  0
 
 const char * const CREATE_DDL =
-    "CREATE TABLE games("
-        "id     INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "result INT NOT NULL"
-    ");";
+"CREATE TABLE games("
+"id     INTEGER PRIMARY KEY AUTOINCREMENT,"
+"result INT NOT NULL"
+");";
 
 const char * const INSERT_DML = 
-    "INSERT INTO games(result) VALUES(?);";
+"INSERT INTO games(result) VALUES(?);";
 
 int acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g, count_t &t);
 
 void parse_options(config &conf, int argc, char *argv[]);
 
 sqlite_ptr open_db(const std::string &c);
+
+stmt_ptr prepare_st(sqlite_ptr &sq, const char *SQL);
 
 void dump_result(sqlite_ptr &db, int result);
 
@@ -52,97 +65,92 @@ int main(int argc, char *argv[]) {
     parse_options(conf, argc, argv);
 
     auto db = open_db(conf.output);
-    
-    if( db == nullptr) {
-       exit(2);
-    }
 
-    sqlite3_stmt *stmt;
-    int result = sqlite3_prepare(db.get(), INSERT_DML, -1, &stmt, nullptr);
-    if( result != SQLITE_OK ) {
-        std::cerr << "PREPARE: " << sqlite3_errstr(result) << "\n";
+    if( db == nullptr) {
         exit(2);
     }
 
-	size_t done = 0;
+    auto stmt = prepare_st(db, INSERT_DML);
 
-	std::istream *inputfile = &std::cin;
+    size_t done = 0;
 
-	std::string s0;
+    std::istream *inputfile = &std::cin;
 
-	count_t white, black, draw, games, stars;
-	white = black = draw = games = stars = 0;
+    std::string s0;
 
-	do{
-		std::getline(*inputfile, s0);
-		result = acc_resut(s0, white, black, draw, games, stars);		
+    count_t white, black, draw, games, stars;
+    white = black = draw = games = stars = 0;
+
+    int result;
+
+    do{
+        std::getline(*inputfile, s0);
+        result = acc_resut(s0, white, black, draw, games, stars);		
         if( result == 100 ) continue;
-		done += s0.size() + 1;
-        result = sqlite3_bind_int( stmt, 1, result);
+        done += s0.size() + 1;
+        result = sqlite3_bind_int( stmt.get(), 1, result);
         if( result != SQLITE_OK ) {
             std::cerr << "BIND: " << sqlite3_errstr(result) << "\n";
             exit(1);
         }
-        result = sqlite3_step( stmt );
+        result = sqlite3_step( stmt.get() );
         if( result != SQLITE_DONE ) {
             std::cerr << "STEP: " << sqlite3_errstr(result) << "\n";
             exit(1);
         }
-        sqlite3_reset(stmt);
-        sqlite3_clear_bindings(stmt);
-	} while( inputfile->good() );
+        sqlite3_reset(stmt.get());
+        sqlite3_clear_bindings(stmt.get());
+    } while( inputfile->good() );
 
-    sqlite3_finalize(stmt);
+    if( inputfile->bad() ) {
+        std::cerr << "Input file critical error" << std::endl;
+        return 1;
+    }
 
-	if( inputfile->bad() ) {
-		std::cerr << "Input file critical error" << std::endl;
-		return 1;
-	}
+    std::cout << "\rGames: " << games << "\n"
+        << "White: " << white
+        << ". Black: " << black
+        << ". Draw: " << draw
+        << ". Unknown: " << stars << std::endl;
 
-	std::cout << "\rGames: " << games << "\n"
-		<< "White: " << white
-		<< ". Black: " << black
-		<< ". Draw: " << draw
-		<< ". Unknown: " << stars << std::endl;
+    if( games != ( white + black + draw + stars ) ) {
+        std::cout << "Warning: games count!" << std::endl;
+    }
 
-	if( games != ( white + black + draw + stars ) ) {
-		std::cout << "Warning: games count!" << std::endl;
-	}
-
-	return 0;
+    return 0;
 }
 
 int acc_resut(const std::string &s, count_t &w, count_t &b, count_t &d, count_t &g, count_t &t) {
-	if( s.starts_with("[Result") ) {
-		g++;
-		auto i = s.find_first_of('-', 9);
-		if( i == s.npos ) {
-			t++;
-			return -1;
-		}
-		auto c = s.at(i - 1);
-		switch(c) {
-			case '1':
-				w++;
-				return RESULT_WHITE;
-			case '0':
-				b++;
-				return RESULT_BLACK;
-			case '2':
-				d++;
-				return RESULT_DRAW;
-		}
+    if( s.starts_with("[Result") ) {
+        g++;
+        auto i = s.find_first_of('-', 9);
+        if( i == s.npos ) {
+            t++;
+            return -1;
+        }
+        auto c = s.at(i - 1);
+        switch(c) {
+            case '1':
+                w++;
+                return RESULT_WHITE;
+            case '0':
+                b++;
+                return RESULT_BLACK;
+            case '2':
+                d++;
+                return RESULT_DRAW;
+        }
 
-	}
+    }
     return 100;
 }
 
 void parse_options(config &conf, int argc, char *argv[]) {
-	cxxopts::Options opts("","");
-	opts.add_options()
+    cxxopts::Options opts("","");
+    opts.add_options()
         ("output", "Database file to dump data. If new requires option --create-script", cxxopts::value<std::string>()->default_value("pgn.dump"));
 
-	auto res = opts.parse(argc, argv);
+    auto res = opts.parse(argc, argv);
 
     conf.output = std::move(res["output"].as<std::string>());
 }
@@ -163,7 +171,19 @@ sqlite_ptr open_db(const std::string &c) {
     }
 
     return sqlite_ptr(db);
-    
+
 }
 
+stmt_ptr prepare_st(sqlite_ptr &db, const char *SQL) {
+    sqlite3_stmt *stmt;
+
+    int result = sqlite3_prepare(db.get(), SQL, -1, &stmt, nullptr);
+    if( result != SQLITE_OK ) {
+        std::cerr << "PREPARE: " << sqlite3_errstr(result) << "\n";
+        exit(2);
+    }
+
+    return stmt_ptr(stmt);
+    
+}
 
